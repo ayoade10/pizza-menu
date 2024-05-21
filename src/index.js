b9eb27df4100c7c3b2f71271c1b99551e49fc960
Abolade <ayoadeabolade862@gmail.com>
2024-05-20 23:03:06 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,176 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    // const style = { color: "red", fontSize: "48px", textTransform: "upperCase" }
+    const style = {};
+
+    return (
+        <header className="header">
+            <h1 style={style} className="header">Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu() {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length
+
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+
+            {numPizzas > 0 ? (
+                // this "<></>" is called react fragment is used in wrapping two element under a parent without traversing the DOM
+                <>
+                    <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious</p>
+
+                    <ul className="pizzas">
+                        {pizzaData.map((pizza) => (
+                            // pizzaObj is a variable that contains information like such as name, ingredients, price, etc.)
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
+                // the : part of this code is basically like the else part of an if statement ": = else"
+            ) : <p>We're working on our menu. Please come back later</p>}
+
+
+
+            {/* <Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />
+
+            <Pizza
+                name="Pizza Finghi"
+                ingredients="Onions, mushrooms, mozerella cheese, tomatos"
+                photoName="pizzas/funghi.jpg"
+                price={13}
+            /> */}
+
+        </main>
+    )
+}
+
+// In react we write new components using functions
+function Pizza({ pizzaObj }) {
+
+    // if (pizzaObj.soldOut) return null;
+
+    return (
+        // pizza under this class name is a CSS class that is condition to function after some requires are met
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+
+                {/* this two performs the same function */}
+
+                {/* {pizzaObj.soldOut ? (
+                    <span>SOLD OUT</span>
+                ) : (
+                    <span>{pizzaObj.price}</span>
+                )} */}
+
+                <span> {pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+            </div>
+        </li >
+    )
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 10;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour
+
+    // if (hour >= openHour && hour <= closeHour) alert("We're Open")
+    // else alert("Sorry we're currently closed")
+    // if (!isOpen) return <p>We are closed</p>
+
+    return (
+        <footer className="footer">
+            {isOpen ? (
+                <Order closeHour={closeHour} openHour={openHour} />
+            ) : <p>We're happy to welcome you between {openHour}:00 to {closeHour}:00</p>}
+        </footer>
+    )
+}
+
+function Order({ closeHour, openHour }) {
+    return (
+        <div className="order">
+            <p>We're open until {openHour}:00 to {closeHour}:00, come visit us or order online. Thank you!</p>
+            <button className="btn">Order</button>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
